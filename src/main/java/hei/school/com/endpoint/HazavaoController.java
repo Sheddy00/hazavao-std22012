1536b0b3bcb81eb4557677569abedf9b3d99c7dd
Sheddy <hei.sheddy.2@gmail.com>
2025-06-13 08:44:52 +0300

    feat: Creation of hazavao endpoint

--- src/main/java/hei/school/com/endpoint/HazavaoController.java
+++ src/main/java/hei/school/com/endpoint/HazavaoController.java
@@ -1,4 +1,22 @@
 package hei.school.com.endpoint;
 
+import hei.school.com.service.ChatGptService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hazavao")
 public class HazavaoController {
+    private final ChatGptService chatGptService;
+
+    public HazavaoController(ChatGptService chatGptService) {
+        this.chatGptService = chatGptService;
+    }
+
+    @GetMapping
+    public String hazavao(@RequestParam String teny) {
+        return chatGptService.getMalagasyDefinition(teny);
+    }
 }
